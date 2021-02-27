d8f6a89d8c58c7cc55f464b425259de742332c7a
Chakrayan <79740371+Chakrayan@users.noreply.github.com>
2021-02-27 17:30:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 	$username="bf1f491c16551f";
 	$password="9c5c709c";
 	$dbname="heroku_cab583369fb7ce6";
-	mysql_connect($server, $username, $password);
-	mysql_select_db($dbname);
+	mysql_connect($server, $username, $password)or die("connect error");
+	mysql_select_db($dbname)or die("database error");
 	
-	 $sql = "SELECT * FROM users ORDER BY username";
-	 $query = mysql_query($sql);
+	$sql = "SELECT * FROM users ORDER BY username";
+	$query = mysql_query($sql);
 	 
-	 echo"<h1>Select database in Heroku</h1>";
-	 
-	 while($data = mysql_fetch_array($query)){
-	 	echo"$data["username"]<br>";
-	 }
+	echo"<h1>Select database in Heroku</h1>";
+
+	while($data = mysql_fetch_array($query)){
+	echo"$data['username']<br>";
+	}
 
 ?>
