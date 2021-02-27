82476f7c878dda958409674651b49e1d087057c4
Chakrayan <79740371+Chakrayan@users.noreply.github.com>
2021-02-27 17:50:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
-<?
+<?php 
 	$server = "us-cdbr-east-03.cleardb.com";
 	$username="bf1f491c16551f";
 	$password="9c5c709c";
 	$dbname="heroku_cab583369fb7ce6";
-	$conn = new mysqli($server, $username, $password, $dbname);
+    // Create connection
+    $connect = new mysqli($server, $username, $password, $dbname);
+
+    // Check Connection
+
+    if ($connect->connect_error) {
+        die("Something wrong.: " . $connect->connect_error);
+      }
+
+    $sql = "SELECT * FROM users";
+    $result = $connect->query($sql);
 	
-
-	$sql = "SELECT * FROM users";
-	$result = mysqli_query($conn, $sql);
-	 
-	echo"<h1>Select database in Heroku</h1>";
-
-	 while ($row = mysqli_fetch_assoc($result)) {
-	 	echo"$row['username']";
-		echo"<br>";
-	 }
-
+	while($row = $result->fetch_assoc()):
+	echo $row['username']; 
+	echo "<br>";
+	endwhile
 ?>
