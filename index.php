1761b1768d926f7efe21864159cd834f246a0231
Chakrayan <79740371+Chakrayan@users.noreply.github.com>
2021-02-27 17:38:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?
-	mysql_connect("us-cdbr-east-03.cleardb.com", "bf1f491c16551f", "9c5c709c")or die("connect error");
-	mysql_select_db("heroku_cab583369fb7ce6")or die("database error");
+	$server = "us-cdbr-east-03.cleardb.com";
+	$username="bf1f491c16551f";
+	$password="9c5c709c";
+	$dbname="heroku_cab583369fb7ce6";
+	$conn = new mysqli($server, $username, $password, $dbname);
 	
+
 	$sql = "SELECT * FROM users";
-	$query = mysql_query($sql)or die("query error");
+	$result = mysqli_query($conn, $sql);
 	 
 	echo"<h1>Select database in Heroku</h1>";
 
-	while($data = mysql_fetch_array($query)){
-	echo"$data['username']<br>";
-	}
+	 while ($row = mysqli_fetch_assoc($result)) {
+	 	echo"$row['username']";
+		echo"<br>";
+	 }
 
 ?>
