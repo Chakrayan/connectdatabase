72d846936565e3dcb6cca064cb684ade64ced72c
Chakrayan <79740371+Chakrayan@users.noreply.github.com>
2021-02-27 17:24:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?
+	$servername = "us-cdbr-east-03.cleardb.com";
+	$username="bf1f491c16551f";
+	$password="9c5c709c";
+	$dbname="heroku_cab583369fb7ce6";
+	mysql_connect($server, $username, $password);
+	mysql_select_db($dbname);
+	
+	 $sql = "SELECT * FROM users ORDER BY username";
+	 $query = mysql_query($sql);
+	 
+	 echo"<h1>Select database in Heroku</h1>";
+	 
+	 while($data = mysql_fetch_array($query)){
+	 		echo"$data[username]<br>";
+	 }
+
+?>
